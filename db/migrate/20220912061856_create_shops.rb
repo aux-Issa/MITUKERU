e5975880ec5bffdd14f9df7f0c17946ca664352d
Issa Itoi <issa1116184028@gmail.com>
2022-09-12 16:44:30 +0900

    add migtation

---
+++ db/migrate/20220912061856_create_shops.rb
@@ -0,0 +1,22 @@
+class CreateShops < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shops do |t|
+      t.references :company, foreign_key: true
+      t.references :city, foreign_key: true
+
+      t.bigint :ieul_shop_id, null: false
+      t.string :postal_code
+      t.string :name
+      t.text :catch_copy, null: false
+      t.string :remaining_address, null: false
+      t.string :telephone_no
+      t.string :fax_no
+      t.string :opening_hour
+      t.string :holiday
+      t.text :introduction, null: false
+      t.text :logo_url, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20220912061906_create_assessable_areas.rb
@@ -0,0 +1,9 @@
+class CreateAssessableAreas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :assessable_areas do |t|
+      t.references :shop, foreign_key: true
+      t.references :city, foreign_key: true
+      t.timestamps
+    end
+  end
+end
