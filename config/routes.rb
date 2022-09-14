639aeaf03ea2bfbe323d8aec54d5a1092c43177d
Takeaki YAMAMOTO <takeaki.yamamoto@gmail.com>
2022-09-14 11:18:08 +0900

    コントローラ関連のrename

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'assessment_request', to: 'assessment_requests#new'
   resources :shops 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
