ccffed4deb231d005c0cc9edc369e9e061fa368f
Takeaki YAMAMOTO <takeaki.yamamoto@gmail.com>
2022-09-14 16:43:40 +0900

    フォームの外形の作成

--- app/controllers/assessment_requests_controller.rb
+++ app/controllers/assessment_requests_controller.rb
@@ -1,4 +1,37 @@
-class AssessmentRequestsController < ApplicationController
+class AssessmentRequestsController< ApplicationController
+
     def new
+      @assessment_request_form= AssessmentRequestForm.new
     end
-end
+  
+    def create
+      @assessment_request_form= AssessmentRequestForm.new(assessment_request_params)
+      debugger
+      if @assessment_request_form.save
+        # 保存の成功処理
+      else
+        render :new
+      end
+    end
+  
+    private
+    def assessment_request_params
+      params.require(:assessment_request).permit(
+          # :branch_id, 
+          :property_city, 
+          :property_address, 
+          :property_type, 
+          :property_exclusive_area, 
+          :property_land_area, 
+          :property_building_area, 
+          :property_building_area_unit, 
+          :property_floor_area, 
+          # :url_param, 
+          :property_room_plan, 
+          :property_constructed_year, 
+          :user_email, 
+          :user_name, 
+          :user_name_kana, 
+          :user_tel)
+    end
+  end
--- app/forms/assessment_request_form.rb
+++ app/forms/assessment_request_form.rb
@@ -2,6 +2,24 @@ class AssessmentRequestForm
     include ActiveModel::Model # 通常のモデルのようにvalidationなどを使えるようにする
     include ActiveModel::Attributes # ActiveRecordのカラムのような属性を加えられるようにする
   
-    attribute :branch_id
-end
-  
+    attribute :branch_id, :integer # 
+    attribute :property_city, :integer
+    attribute :property_address, :string
+    attribute :property_type, :integer # 1 ~ 3
+    attribute :property_exclusive_area, :float
+    attribute :property_land_area, :float
+    attribute :property_building_area, :float
+    attribute :property_building_area_unit, :integer # 1 ~ 2
+    attribute :property_floor_area, :float
+    attribute :url_param, :string #
+    attribute :property_room_plan, :integer # 1 ~ 13
+    attribute :property_constructed_year, :integer
+    attribute :user_email
+    attribute :user_name
+    attribute :user_name_kana
+    attribute :user_tel
+
+    def save
+      true
+    end
+  end
