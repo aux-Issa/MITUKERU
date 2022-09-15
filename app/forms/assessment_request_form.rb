f22b0ac6691623886f4f655d846b568c35d4f9ba
Takeaki YAMAMOTO <takeaki.yamamoto@gmail.com>
2022-09-15 17:02:19 +0900

    delete assessment_request_form.rb

--- app/forms/assessment_request_form.rb
+++
@@ -1,7 +0,0 @@
-class RequestForm
-    include ActiveModel::Model # 通常のモデルのようにvalidationなどを使えるようにする
-    include ActiveModel::Attributes # ActiveRecordのカラムのような属性を加えられるようにする
-  
-    attribute :branch_id
-end
-  
