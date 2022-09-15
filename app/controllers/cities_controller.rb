068cd931bbea9c939c1820127d100b27d368c536
Issa Itoi <issa1116184028@gmail.com>
2022-09-15 18:26:58 +0900

    修正: 査定エリアを通じてshopを取得

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -4,6 +4,6 @@ class CitiesController < ApplicationController
   def show
     city_id = params[:id]
     @city = City.find_by(id: city_id)
-    @shops = Shop.where(city_id:)
+    @shops = @city.shops
   end
 end
