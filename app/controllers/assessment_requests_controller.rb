229e36ea1181268ef586ad21e937c354bc84149f
Takeaki YAMAMOTO <takeaki.yamamoto@gmail.com>
2022-09-16 15:10:56 +0900

    apiを叩けるようにした

--- app/controllers/assessment_requests_controller.rb
+++ app/controllers/assessment_requests_controller.rb
@@ -12,7 +12,18 @@ class AssessmentRequestsController< ApplicationController
       @assessment_request= AssessmentRequest.new(assessment_request_params)
       @shop = Shop.find(@assessment_request.branch_id)
       if @assessment_request.valid?
-        # API を叩く処理
+
+        require 'net/http'
+        require 'uri'
+        uri = 'https://miniul-api.herokuapp.com/affiliate/v2/conversions'
+        uri = URI.parse(uri)
+        header = {'Content-Type': 'application/json'}
+        param = @assessment_request.attributes
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        response = http.request_post(uri, param.to_json, header)
+        logger.debug(response)
         redirect_to assessment_requests_success_path
       else
         flash[:error_messages] = @assessment_request.errors.full_messages
