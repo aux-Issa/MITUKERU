b2327b2d39725772b299defc4039ca7a074523da
Takeaki YAMAMOTO <takeaki.yamamoto@gmail.com>
2022-09-16 19:16:33 +0900

    コードのフォーマットを整えた

--- app/controllers/assessment_requests_controller.rb
+++ app/controllers/assessment_requests_controller.rb
@@ -1,62 +1,62 @@
+# frozen_string_literal: true
 
-class AssessmentRequestsController< ApplicationController
+class AssessmentRequestsController < ApplicationController
+  def thx; end
 
-    def thx
-    end
+  def new
+    @assessment_request = AssessmentRequest.new
+    @shop = Shop.find(params[:shop_id])
+  end
 
-    def new
-      @assessment_request= AssessmentRequest.new
-      @shop = Shop.find(params[:shop_id])
-    end
-  
-    def create
-      @assessment_request= AssessmentRequest.new(assessment_request_params)
-      @shop = Shop.find(@assessment_request.branch_id)
-      if @assessment_request.valid?
+  def create
+    @assessment_request = AssessmentRequest.new(assessment_request_params)
+    @shop = Shop.find(@assessment_request.branch_id)
+    if @assessment_request.valid?
 
-        response = post_assessment_request(@assessment_request)
-        logger.debug(response)
-       
-        redirect_to assessment_requests_success_path
-      else
-        render 'new', status: :unprocessable_entity
-      end
-    end
-  
-    private
-    def assessment_request_params
-      params.require(:assessment_request).permit(
-          :branch_id, 
-          :property_city, 
-          :property_address, 
-          :property_type, 
-          :property_exclusive_area, 
-          :property_land_area, 
-          :property_building_area, 
-          :property_building_area_unit, 
-          :property_floor_area, 
-          :url_param, 
-          :property_room_plan, 
-          :property_constructed_year, 
-          :user_email, 
-          :user_name, 
-          :user_name_kana, 
-          :user_tel)
-    end
-  
-    def post_assessment_request(assessment_request)
-      require 'net/http'
-      require 'uri'
+      response = post_assessment_request(@assessment_request)
+      logger.debug(response)
 
-      uri = 'https://miniul-api.herokuapp.com/affiliate/v2/conversions'
-      uri = URI.parse(uri)
-      header = {'Content-Type': 'application/json'}
-      param = assessment_request.attributes
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-
-      response = http.request_post(uri, param.to_json, header)
-      return response
+      redirect_to assessment_requests_success_path
+    else
+      render 'new', status: :unprocessable_entity
     end
   end
+
+  private
+
+  def assessment_request_params
+    params.require(:assessment_request).permit(
+      :branch_id,
+      :property_city,
+      :property_address,
+      :property_type,
+      :property_exclusive_area,
+      :property_land_area,
+      :property_building_area,
+      :property_building_area_unit,
+      :property_floor_area,
+      :url_param,
+      :property_room_plan,
+      :property_constructed_year,
+      :user_email,
+      :user_name,
+      :user_name_kana,
+      :user_tel
+    )
+  end
+
+  def post_assessment_request(assessment_request)
+    require 'net/http'
+    require 'uri'
+
+    uri = 'https://miniul-api.herokuapp.com/affiliate/v2/conversions'
+    uri = URI.parse(uri)
+    header = { 'Content-Type': 'application/json' }
+    param = assessment_request.attributes
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    http.request_post(uri, param.to_json, header)
+  end
+end
