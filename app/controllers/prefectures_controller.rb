2df23825e3aa7f70802d74441a9f7224509cac3b
Takeaki YAMAMOTO <takeaki.yamamoto@gmail.com>
2022-09-19 15:14:07 +0900

    TODOを追記

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -2,6 +2,7 @@
 
 class PrefecturesController < ApplicationController
   def show
+    # TODO: Citiesコントローラで表示する内容と全く一緒なのでpartial化を検討
     prefecture_id = params[:id]
     cities = City.where(prefecture_id:)
 
