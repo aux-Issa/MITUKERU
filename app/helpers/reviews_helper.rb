c8ffb35d58c5dcfb53560b68be514faa725bf4b1
Issa Itoi <issa1116184028@gmail.com>
2022-09-19 15:31:47 +0900

    rubocop

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,6 +2,6 @@
 
 module ReviewsHelper
   def diff_month(date1, date2)
-    (date2.year * 12 + date2.month) - (date1.year * 12 + date1.month) 
+    ((date2.year * 12) + date2.month) - ((date1.year * 12) + date1.month)
   end
 end
